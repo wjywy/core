221edaf4891a1193435e04a8a4135b9ed33bd7bf
wjywy <2934610933@qq.com>
2023-10-10 23:23:48 +0800

    fix: delete console information and give switch to choose recursive

--- packages/file-service/src/node/disk-file-system.provider.ts
+++ packages/file-service/src/node/disk-file-system.provider.ts
@@ -67,6 +67,8 @@ export interface IWatcher {
 
 @Injectable({ multiple: true })
 export class DiskFileSystemProvider extends RPCService<IRPCDiskFileSystemProvider> implements IDiskFileProvider {
+  private recursive: boolean;
+
   private fileChangeEmitter = new Emitter<FileChangeEvent>();
 
   private watcherServer: UnRecursiveFileSystemWatcher | FileSystemWatcherServer;
@@ -97,6 +99,7 @@ export class DiskFileSystemProvider extends RPCService<IRPCDiskFileSystemProvide
     this.logger = this.loggerManager.getLogger(SupportLogNamespace.Node);
     this.recursive = recursive;
     this.initWatchServer();
+    this.recursive = recursive;
   }
 
   get whenReady() {
@@ -135,9 +138,7 @@ export class DiskFileSystemProvider extends RPCService<IRPCDiskFileSystemProvide
   async watch(uri: UriComponents, options?: { excludes?: string[] }): Promise<number> {
     await this.whenReady;
     const _uri = Uri.revive(uri);
-    // eslint-disable-next-line no-console
-    console.log(options, 'optionsOptionsOptions');
-    const id = await this.watcherServer.watchFileChanges('d:\\front_many\\firstIssue\\core\\tools\\workspace\\front', {
+    const id = await this.watcherServer.watchFileChanges(_uri.toString(), {
       excludes: options?.excludes ?? [],
     });
     const disposable = {
